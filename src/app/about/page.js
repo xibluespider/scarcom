3235e0d414ac278bace0945f1fd6d3646da4f0f9
xibluespider <xibluespider@gmail.com>
2024-11-05 18:47:34 +0530

    about/page.js : add

---
+++ src/app/about/page.js
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-2xl">this is an about page </div>
+    </div>
+  );
+}
