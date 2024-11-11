9a81c4a3bd52b0bd690c3173e91d0fceec103bdc
xibluespider <xibluespider@gmail.com>
2024-11-11 21:05:17 +0530

    npx drizzle-kit generate

---
+++ src/migrations/0000_loud_hiroim.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"email" text NOT NULL,
+	"name" text NOT NULL,
+	"password" text NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
