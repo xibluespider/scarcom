5987c65e099a08787239d428c3b9d05e34cc6cd9
xibluespider <xibluespider@gmail.com>
2024-11-11 17:30:22 +0530

    db.js : add getUserByEmail function

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,19 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
+import { users } from "./schema";
+
+import { eq } from "drizzle-orm";
+
 const pool = postgres(process.env.DATABASE_URL, { max: 1 });
 export const db = drizzle(pool);
+
+export async function getUserByEmail(email) {
+  try {
+    const result = await db.select().from(users).where(eq(users.email, email));
+    return result;
+  } catch (error) {
+    const ierror = { message: "error at getUserByEmail" };
+    throw new Error(ierror);
+  }
+}
