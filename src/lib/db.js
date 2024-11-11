63ad38fa1a0848f1c9a899ab927dcd2c4e60b5a1
xibluespider <xibluespider@gmail.com>
2024-11-11 17:26:33 +0530

    set db.js

---
+++ src/lib/db.js
@@ -0,0 +1,5 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+const pool = postgres(process.env.DATABASE_URL, { max: 1 });
+export const db = drizzle(pool);
