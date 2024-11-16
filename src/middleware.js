17c2d3c038218eaa911a416d54332d612eebab48
xibluespider <xibluespider@gmail.com>
2024-11-16 17:30:48 +0530

    middleware.js : add

---
+++ src/middleware.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { auth } from "./auth";
+
+export const imiddleware = (req) => {
+  const isAuthenticated = !!req.auth;
+  const pathname = req.nextUrl.pathname;
+
+  if (pathname === "/" && !isAuthenticated) {
+    return NextResponse.redirect(new URL("/auth", req.url));
+  }
+
+  if (pathname === "/auth" && isAuthenticated) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+};
+
+export default auth(imiddleware);
+
+export const config = {
+  matcher: ["/", "/auth", "/about"],
+};
