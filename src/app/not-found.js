d26b1fd27c6e2a91b38e91ef119ec04294cc38d6
xibluespider <xibluespider@gmail.com>
2024-11-18 20:35:58 +0530

    not-found.js : add

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col justify-center items-center grow">
+      <div className="flex space-x-5 text-2xl">
+        <div className="font-extralight">404</div>
+        <div>|</div>
+        <p className="font-thin">Not Found</p>
+      </div>
+    </div>
+  );
+}
