88395bcc64e934c311f5e84dcf5e62e567e93f00
xibluespider <xibluespider@gmail.com>
2024-11-18 20:12:03 +0530

    Header.js : add

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@nextui-org/react";
+
+import { LogIn, LogOut } from "lucide-react";
+
+import AppLogo from "../../public/bird.jpg";
+
+export default function Header() {
+  const session = useSession();
+  const isAuthenticated = session.status === "authenticated";
+
+  return (
+    <div className="flex items-center justify-between px-3 py-2">
+      <Link
+        href={"/"}
+        className="flex items-center space-x-2 hover:underline hover:underline-offset-4"
+      >
+        <Image
+          src={AppLogo}
+          alt="app-logo"
+          className="max-w-[30px] rounded-lg"
+        />
+        <div>Scarcom</div>
+      </Link>
+      <div className="flex items-center space-x-4">
+        <Link
+          className="hover:underline hover:underline-offset-4"
+          href={"/about"}
+        >
+          About
+        </Link>
+        {isAuthenticated ? (
+          <Button size="sm" isIconOnly color="primary" onClick={signOut}>
+            <LogOut size={12} color="black" />
+          </Button>
+        ) : null}
+      </div>
+    </div>
+  );
+}
