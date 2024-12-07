1bd52130f25a3956ebcb6433fbea70e8ebbfe13e
xibluespider <xibluespider@gmail.com>
2024-12-07 15:11:11 +0530

    feat: add auth page with placeholder

---
+++ src/app/auth/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div>auth page placeholder</div>
+    </div>
+  );
+}
