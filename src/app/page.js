28881ab49735169d4ea8e00a073e26e4fd154441
xibluespider <xibluespider@gmail.com>
2024-12-07 13:43:10 +0530

    test: verify postgres connection setup with version check

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
-export default function Home() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="text-2xl">scarcom</div>
-    </div>
-  );
+import postgres from "postgres";
+
+async function getData() {
+  const sql = postgres(process.env.DATABASE_URL, { ssl: "require" });
+  const response = await sql`SELECT version()`;
+  return response[0].version;
+}
+
+export default async function Page() {
+  const data = await getData();
+  return <>{data}</>;
 }
