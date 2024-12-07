79bbb70d7f86339980a308d45ac5b0076a9cb4b1
xibluespider <xibluespider@gmail.com>
2024-12-07 19:45:05 +0530

    feat: implement ui for signin and signup

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -1,7 +1,78 @@
+"use client";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <div>auth page placeholder</div>
+      <Tabs defaultValue="signin" className="w-full max-w-[400px] p-2">
+        <TabsList className="w-full justify-evenly">
+          <TabsTrigger value="signin" className="grow">
+            Sign in
+          </TabsTrigger>
+          <TabsTrigger value="signup" className="grow">
+            Sign up
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="signin">
+          <Card>
+            <CardHeader>
+              <CardTitle>Sign in</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="name">Email</Label>
+                <Input id="email" type="email" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="grow">Sign in</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="signup">
+          <Card className="sm:min-w-[400px]">
+            <CardHeader>
+              <CardTitle>Sign up</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" type="text" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="confirm-password">Confirm Password</Label>
+                <Input id="confirm-password" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="grow">Sign up</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
