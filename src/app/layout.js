98bb689cabca9f5e93bb12d952ade42230b3640a
xibluespider <xibluespider@gmail.com>
2024-12-07 13:09:11 +0530

    fix: add suppressHydrationWarning for ThemeProvider error

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
