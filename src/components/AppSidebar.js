9a2d8fe58b964a3cac1962c9d32ee0b5e200fa00
xibluespider <xibluespider@gmail.com>
2024-12-10 11:48:35 +0530

    fix: AppSidebar: transition from demo to application use

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,67 +1,76 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+"use client";
+
+import {
+  LogOutIcon as SignOutIcon,
+  Info as AboutIcon,
+  Loader2 as LoaderIcon,
+} from "lucide-react";
 
 import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarFooter,
 } from "@/components/ui/sidebar";
 
-// Menu items.
-const items = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-];
+import Image from "next/image";
+import Link from "next/link";
+
+import AppLogo from "../../public/bird.jpg";
+
+import useAuthEvents from "@/hooks/useAuthEvents";
 
 export function AppSidebar() {
+  const { isLoading, handleSignOutEvent } = useAuthEvents();
+
   return (
     <Sidebar variant="floating">
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href="/">
+                    <Image
+                      src={AppLogo}
+                      className="size-6 rounded-sm"
+                      alt="applogo"
+                    />
+                    <p>Home</p>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/about">
+                <AboutIcon />
+                <p>About</p>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <SidebarMenuButton onClick={handleSignOutEvent}>
+              {isLoading ? (
+                <LoaderIcon className="animate-spin" />
+              ) : (
+                <SignOutIcon />
+              )}
+              <p>Sign out</p>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 }
