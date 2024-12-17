bedefe9d8516c46e143fcabae8cd4909ef228ac9
xibluespider <xibluespider@gmail.com>
2024-12-17 13:04:27 +0530

    feat: handleSignUp: implement server action

---
+++ src/actions/handleSignUp.js
@@ -0,0 +1,49 @@
+"use server";
+
+import { addUser, getUserByEmail } from "@/lib/db";
+
+export default async function handleSignUp(credentials) {
+  const { name, email, password, confirmPassword } = credentials;
+
+  const handleGetUserError = (e) => {
+    console.log("handleSignUp/getUserByEmail : error caught");
+
+    const error_message = "Internal server error. Please try again later";
+    return { error_message };
+  };
+  const iuser = await getUserByEmail(email).catch(handleGetUserError);
+  console.log(iuser);
+
+  if (iuser?.error_message) return { ok: false, message: iuser.error_message };
+
+  if (iuser)
+    return {
+      ok: false,
+      message: "User already exists. Please sign in",
+    };
+
+  if (password !== confirmPassword)
+    return {
+      ok: false,
+      message: "Passwords do not match. Please sign up again",
+    };
+
+  const handleAddUserError = (error) => {
+    console.log("handleSignUp/addUser : error caught");
+
+    return {
+      ok: false,
+      message: "Internal server error. Please try again later",
+    };
+  };
+  const response = await addUser(credentials).catch(handleAddUserError);
+  console.log(response);
+
+  if (!response)
+    return {
+      ok: true,
+      message: "User account created. Please sign in",
+    };
+
+  return response;
+}
