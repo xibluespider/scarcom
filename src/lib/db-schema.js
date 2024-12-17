38adc5ba7286f034479a64e5bc033cbb9dd44f05
xibluespider <xibluespider@gmail.com>
2024-12-17 13:03:23 +0530

    feat: db-schema: add users schema

---
+++ src/lib/db-schema.js
@@ -0,0 +1,8 @@
+import { pgTable, text, uuid } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  email: text("email").notNull().unique(),
+  name: text("name").notNull(),
+  password: text("password").notNull(),
+});
