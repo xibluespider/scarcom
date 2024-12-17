e1530cdd7b2cf80fde5c8eb69a4b5084aae91167
xibluespider <xibluespider@gmail.com>
2024-12-17 13:14:19 +0530

    feat: zod-schema: add signin schema

--- src/lib/zod-schema.js
+++ src/lib/zod-schema.js
@@ -15,3 +15,10 @@ export const signUpSchema = z
     message: "Passwords don't match",
     path: ["confirmPassword"],
   });
+
+export const signInSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" }),
+});
