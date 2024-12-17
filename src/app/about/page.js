9f0952fd08a01709255934ae85061dbaeb2bb7a3
xibluespider <xibluespider@gmail.com>
2024-12-17 22:04:34 +0530

    feat: add about page

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Page() {
+  return (
+    <div className="grow flex items-center justify-center">
+      <div>about page placeholder</div>
+    </div>
+  );
+}
