07ad127d9a11e0504a60e8df3f3ae4b009bbc88d
xibluespider <xibluespider@gmail.com>
2024-12-17 16:34:20 +0530

    test: app/page: verify handleSignOutEvent

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,22 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+
 import { useSession } from "next-auth/react";
+
+import useAuthEvents from "@/hooks/useAuthEvents";
 
 export default function Page() {
   const session = useSession();
 
+  const { handleSignOutEvent, isLoading } = useAuthEvents();
+
   return (
     <div>
       <div>{JSON.stringify(session)}</div>
+      <Button onClick={handleSignOutEvent}>
+        {isLoading ? "loading" : "sign out"}
+      </Button>
     </div>
   );
 }
