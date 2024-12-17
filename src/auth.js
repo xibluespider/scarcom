b3b6605808495e9536fc54f3792494a0c45fbf18
xibluespider <xibluespider@gmail.com>
2024-12-17 13:16:55 +0530

    feat: setup auth.js and route

---
+++ src/auth.js
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+import { getUserByEmail } from "./lib/db";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const { email, password } = credentials;
+
+        if (!email || !password) throw new Error("Missing Credentials");
+
+        const handleGetUserError = () => {
+          console.log("auth/authorize/getUserByEmail : error caught");
+          const message = "Internal Server error. Please try again later";
+          throw new Error(message);
+        };
+        const iuser = await getUserByEmail(email).catch(handleGetUserError);
+
+        if (!iuser) throw new Error("User not found");
+
+        if (iuser.password !== password) throw new Error("Invalid credentials");
+
+        const { password: omitted, ...safeUser } = iuser;
+        return safeUser;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth",
+  },
+  secret: process.env.AUTH_SECRET,
+});
