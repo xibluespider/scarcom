48d89738a272a1053cba4a996f7af754fcf692af
xibluespider <xibluespider@gmail.com>
2024-12-17 13:03:36 +0530

    feat: zod-schema: add signup schema

---
+++ src/lib/zod-schema.js
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const signUpSchema = z
+  .object({
+    name: z.string().min(1, { message: "Name is required" }),
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters" }),
+    confirmPassword: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
