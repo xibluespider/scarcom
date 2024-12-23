4e145f37ea39a901853f82eaade8e043ebef2b31
xibluespider <xibluespider@gmail.com>
2024-12-23 13:00:07 +0530

    fix: handleSignUp: use try/catch

--- src/actions/handleSignUp.js
+++ src/actions/handleSignUp.js
@@ -4,60 +4,50 @@ import { addUser, getUserByEmail } from "@/lib/db";
 import bcrypt from "bcryptjs";
 
 export default async function handleSignUp(credentials) {
-  const { name, email, password, confirmPassword } = credentials;
+	const { name, email, password, confirmPassword } = credentials;
 
-  const handleGetUserError = (e) => {
-    console.log("handleSignUp/getUserByEmail : error caught");
+	let iuser = null;
+	try {
+		iuser = await getUserByEmail(email);
+	} catch (error) {
+		return {
+			ok: false,
+			message: "Internal server error. Please try again later",
+		};
+	}
 
-    const error_message = "Internal server error. Please try again later";
-    return { error_message };
-  };
-  const iuser = await getUserByEmail(email).catch(handleGetUserError);
-  console.log(iuser);
+	if (iuser)
+		return {
+			ok: false,
+			message: "User already exists. Please sign in",
+		};
 
-  if (iuser?.error_message) return { ok: false, message: iuser.error_message };
+	if (password !== confirmPassword)
+		return {
+			ok: false,
+			message: "Passwords do not match. Please sign up again",
+		};
 
-  if (iuser)
-    return {
-      ok: false,
-      message: "User already exists. Please sign in",
-    };
+	const saltRounds = parseInt(process.env.SALT_ROUNDS);
+	const hashedPassword = bcrypt.hashSync(password, saltRounds);
 
-  if (password !== confirmPassword)
-    return {
-      ok: false,
-      message: "Passwords do not match. Please sign up again",
-    };
+	const new_user_credentials = {
+		email,
+		name,
+		password: hashedPassword,
+	};
 
-  const hashedPassword = bcrypt.hashSync(
-    password,
-    parseInt(process.env.SALT_ROUNDS)
-  );
+	try {
+		const response = await addUser(new_user_credentials);
+	} catch (error) {
+		return {
+			ok: false,
+			message: "Internal server error. Please try again later",
+		};
+	}
 
-  const new_user_credentials = {
-    email,
-    name,
-    password: hashedPassword,
-  };
-
-  const handleAddUserError = (error) => {
-    console.log("handleSignUp/addUser : error caught");
-
-    return {
-      ok: false,
-      message: "Internal server error. Please try again later",
-    };
-  };
-  const response = await addUser(new_user_credentials).catch(
-    handleAddUserError
-  );
-  console.log(response);
-
-  if (!response)
-    return {
-      ok: true,
-      message: "User account created. Please sign in",
-    };
-
-  return response;
+	return {
+		ok: true,
+		message: "User account created. Please sign in",
+	};
 }
