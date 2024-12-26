f4bff3ff9743b4c8a385efa1dcd7bc7de036992b
xibluespider <xibluespider@gmail.com>
2024-12-26 20:20:26 +0530

    feat: add not-found page

---
+++ src/app/not-found.js
@@ -0,0 +1,16 @@
+"use client";
+
+export default function Page() {
+	return (
+		<div className="grow border rounded-lg m-2 flex items-center justify-center">
+			<h1 className="border-r border-gray-300 mr-5 pr-5 text-2xl font-medium leading-[49px]">
+				404
+			</h1>
+			<div>
+				<h2 className="text-sm font-normal leading-[49px]">
+					This page could not be found.
+				</h2>
+			</div>
+		</div>
+	);
+}
