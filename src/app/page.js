6ab69169a3d34b21be5c5d23a4f95b18a4ee1ed5
xibluespider <xibluespider@gmail.com>
2024-12-26 20:28:09 +0530

    feat: app/page: add initial view

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 "use client";
 
+import Image from "next/image";
+import AppLogo from "../../public/bird.jpg";
+
 export default function Page() {
-  return (
-    <div className="grow flex items-center justify-center">
-      <div>chat area placeholder</div>
-    </div>
-  );
+	return (
+		<div className="grow border rounded-lg m-2 flex flex-col items-center justify-center space-y-3">
+			<Image src={AppLogo} alt="AppLogo" className="max-w-[80px] rounded-2xl" />
+			<div className="text-xl">kindly select a conversation</div>
+		</div>
+	);
 }
