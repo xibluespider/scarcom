f7f76d5b67da87d2755dd8e55922a8f6be888af4
xibluespider <xibluespider@gmail.com>
2024-12-26 22:01:34 +0530

    chore: AppSidebar: prettier format

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,22 +1,22 @@
 "use client";
 
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarFooter,
-  SidebarTrigger,
-  useSidebar,
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarFooter,
+	SidebarTrigger,
+	useSidebar,
 } from "@/components/ui/sidebar";
 
 import {
-  Info as AboutIcon,
-  Loader2 as LoadingIcon,
-  LogOutIcon as SignOutIcon,
+	Info as AboutIcon,
+	Loader2 as LoadingIcon,
+	LogOutIcon as SignOutIcon,
 } from "lucide-react";
 
 import Image from "next/image";
@@ -29,75 +29,75 @@ import { useSession } from "next-auth/react";
 import useAuthEvents from "@/hooks/useAuthEvents";
 
 export function AppSidebar() {
-  const { isLoading, handleSignOutEvent } = useAuthEvents();
+	const { isLoading, handleSignOutEvent } = useAuthEvents();
 
-  const { isMobile } = useSidebar();
+	const { isMobile } = useSidebar();
 
-  const session = useSession();
-  const statusIsLoading = isLoading || session.status === "loading";
+	const session = useSession();
+	const statusIsLoading = isLoading || session.status === "loading";
 
-  return (
-    <div>
-      <SidebarTrigger
-        variant="outline"
-        className={!isMobile ? "hidden" : "absolute top-1 left-1 size-10"}
-      />
-      <Sidebar variant="floating" collapsible="icon">
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                <SidebarMenuItem>
-                  <SidebarMenuButton
-                    asChild
-                    size="lg"
-                    tooltip={{ children: "Home", sideOffset: 15 }}
-                  >
-                    <Link href="/">
-                      <Image
-                        src={AppLogo}
-                        className="size-6 ml-1 rounded-sm"
-                        alt="applogo"
-                      />
-                      <p>Home</p>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </SidebarContent>
-        <SidebarFooter>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton
-                asChild
-                tooltip={{ children: "About", sideOffset: 15 }}
-              >
-                <Link href="/about">
-                  <AboutIcon />
-                  <p>About</p>
-                </Link>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-            <SidebarMenuItem>
-              <SidebarMenuButton
-                onClick={handleSignOutEvent}
-                className={session.status === "unauthenticated" ? "hidden" : ""}
-                tooltip={{ children: "Sign out", sideOffset: 15 }}
-              >
-                <LoadingIcon
-                  className={
-                    statusIsLoading ? "animate-spin mx-auto" : "hidden"
-                  }
-                />
-                <SignOutIcon className={statusIsLoading ? "hidden" : ""} />
-                <p className={statusIsLoading ? "hidden" : ""}>Sign out</p>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarFooter>
-      </Sidebar>
-    </div>
-  );
+	return (
+		<div>
+			<SidebarTrigger
+				variant="outline"
+				className={!isMobile ? "hidden" : "absolute top-1 left-1 size-10"}
+			/>
+			<Sidebar variant="floating" collapsible="icon">
+				<SidebarContent>
+					<SidebarGroup>
+						<SidebarGroupContent>
+							<SidebarMenu>
+								<SidebarMenuItem>
+									<SidebarMenuButton
+										asChild
+										size="lg"
+										tooltip={{ children: "Home", sideOffset: 15 }}
+									>
+										<Link href="/">
+											<Image
+												src={AppLogo}
+												className="size-6 ml-1 rounded-sm"
+												alt="applogo"
+											/>
+											<p>Home</p>
+										</Link>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							</SidebarMenu>
+						</SidebarGroupContent>
+					</SidebarGroup>
+				</SidebarContent>
+				<SidebarFooter>
+					<SidebarMenu>
+						<SidebarMenuItem>
+							<SidebarMenuButton
+								asChild
+								tooltip={{ children: "About", sideOffset: 15 }}
+							>
+								<Link href="/about">
+									<AboutIcon />
+									<p>About</p>
+								</Link>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+						<SidebarMenuItem>
+							<SidebarMenuButton
+								onClick={handleSignOutEvent}
+								className={session.status === "unauthenticated" ? "hidden" : ""}
+								tooltip={{ children: "Sign out", sideOffset: 15 }}
+							>
+								<LoadingIcon
+									className={
+										statusIsLoading ? "animate-spin mx-auto" : "hidden"
+									}
+								/>
+								<SignOutIcon className={statusIsLoading ? "hidden" : ""} />
+								<p className={statusIsLoading ? "hidden" : ""}>Sign out</p>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+					</SidebarMenu>
+				</SidebarFooter>
+			</Sidebar>
+		</div>
+	);
 }
