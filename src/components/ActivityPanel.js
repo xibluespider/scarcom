615af62893ec9b65f7b362e0b59e57e560d0a7ff
xibluespider <xibluespider@gmail.com>
2024-12-27 14:23:33 +0530

    chore: add ActivityPanel placeholder
    fix: style: ActivityPanel: adjust margin on placeholder

---
+++ src/components/ActivityPanel.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ActivityPanel() {
+	return (
+		<div className="m-2 grow border rounded-lg flex flex-col items-center justify-center">
+			<div className="text-xl">ActivityPanel</div>
+		</div>
+	);
+}
