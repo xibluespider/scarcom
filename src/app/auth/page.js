0e381e22c35ee3b97a2e21d1485316ac2872efe3
xibluespider <xibluespider@gmail.com>
2024-12-27 14:04:54 +0530

    chore: auth/page: prettier format

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -4,11 +4,11 @@ import useAuthEvents from "@/hooks/useAuthEvents";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -16,122 +16,122 @@ import { Button } from "@/components/ui/button";
 import { Loader2 as LoadingIcon } from "lucide-react";
 
 export default function Page() {
-  const {
-    handleSignUpFormSubmit,
-    signUpRegister,
-    signUpErrors,
-    handleSignInFormSubmit,
-    signInRegister,
-    signInErrors,
-    isLoading,
-  } = useAuthEvents();
+	const {
+		handleSignUpFormSubmit,
+		signUpRegister,
+		signUpErrors,
+		handleSignInFormSubmit,
+		signInRegister,
+		signInErrors,
+		isLoading,
+	} = useAuthEvents();
 
-  return (
-    <div className="grow flex flex-col items-center justify-center">
-      <Tabs defaultValue="signin" className="w-full max-w-[400px] p-2">
-        <TabsList className="w-full justify-evenly">
-          <TabsTrigger value="signin" className="grow">
-            Sign in
-          </TabsTrigger>
-          <TabsTrigger value="signup" className="grow">
-            Sign up
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="signin">
-          <form onSubmit={handleSignInFormSubmit}>
-            <Card>
-              <CardHeader>
-                <CardTitle>Sign in</CardTitle>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <div className="space-y-1">
-                  <Label htmlFor="name">Email</Label>
-                  <Input
-                    name="email"
-                    type="email"
-                    {...signInRegister("email")}
-                    autoComplete="username"
-                    error={signInErrors.email?.message}
-                  />
-                </div>
-                <div className="space-y-1">
-                  <Label htmlFor="password">Password</Label>
-                  <Input
-                    name="password"
-                    type="password"
-                    {...signInRegister("password")}
-                    autoComplete="current-password"
-                    error={signInErrors.password?.message}
-                  />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button className="grow" type="submit" disabled={isLoading}>
-                  {isLoading ? <LoadingIcon className="animate-spin" /> : null}
-                  Sign in
-                </Button>
-              </CardFooter>
-            </Card>
-          </form>
-        </TabsContent>
-        <TabsContent value="signup">
-          <form onSubmit={handleSignUpFormSubmit}>
-            <Card>
-              <CardHeader>
-                <CardTitle>Sign up</CardTitle>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <div className="space-y-1">
-                  <Label htmlFor="name">Name</Label>
-                  <Input
-                    name="name"
-                    type="text"
-                    {...signUpRegister("name")}
-                    autoComplete="username"
-                    error={signUpErrors.name?.message}
-                  />
-                </div>
-                <div className="space-y-1">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    name="email"
-                    type="email"
-                    {...signUpRegister("email")}
-                    autoComplete="username"
-                    error={signUpErrors.email?.message}
-                  />
-                </div>
-                <div className="space-y-1">
-                  <Label htmlFor="password">Password</Label>
-                  <Input
-                    name="password"
-                    type="password"
-                    {...signUpRegister("password")}
-                    autoComplete="current-password"
-                    error={signUpErrors.password?.message}
-                  />
-                </div>
-                <div className="space-y-1">
-                  <Label htmlFor="confirmPassword">Confirm Password</Label>
-                  <Input
-                    name="confirm-password"
-                    type="password"
-                    {...signUpRegister("confirmPassword")}
-                    autoComplete="current-password"
-                    error={signUpErrors.confirmPassword?.message}
-                  />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button className="grow" type="submit" disabled={isLoading}>
-                  {isLoading ? <LoadingIcon className="animate-spin" /> : null}
-                  Sign up
-                </Button>
-              </CardFooter>
-            </Card>
-          </form>
-        </TabsContent>
-      </Tabs>
-    </div>
-  );
+	return (
+		<div className="grow flex flex-col items-center justify-center">
+			<Tabs defaultValue="signin" className="w-full max-w-[400px] p-2">
+				<TabsList className="w-full justify-evenly">
+					<TabsTrigger value="signin" className="grow">
+						Sign in
+					</TabsTrigger>
+					<TabsTrigger value="signup" className="grow">
+						Sign up
+					</TabsTrigger>
+				</TabsList>
+				<TabsContent value="signin">
+					<form onSubmit={handleSignInFormSubmit}>
+						<Card>
+							<CardHeader>
+								<CardTitle>Sign in</CardTitle>
+							</CardHeader>
+							<CardContent className="space-y-2">
+								<div className="space-y-1">
+									<Label htmlFor="name">Email</Label>
+									<Input
+										name="email"
+										type="email"
+										{...signInRegister("email")}
+										autoComplete="username"
+										error={signInErrors.email?.message}
+									/>
+								</div>
+								<div className="space-y-1">
+									<Label htmlFor="password">Password</Label>
+									<Input
+										name="password"
+										type="password"
+										{...signInRegister("password")}
+										autoComplete="current-password"
+										error={signInErrors.password?.message}
+									/>
+								</div>
+							</CardContent>
+							<CardFooter>
+								<Button className="grow" type="submit" disabled={isLoading}>
+									{isLoading ? <LoadingIcon className="animate-spin" /> : null}
+									Sign in
+								</Button>
+							</CardFooter>
+						</Card>
+					</form>
+				</TabsContent>
+				<TabsContent value="signup">
+					<form onSubmit={handleSignUpFormSubmit}>
+						<Card>
+							<CardHeader>
+								<CardTitle>Sign up</CardTitle>
+							</CardHeader>
+							<CardContent className="space-y-2">
+								<div className="space-y-1">
+									<Label htmlFor="name">Name</Label>
+									<Input
+										name="name"
+										type="text"
+										{...signUpRegister("name")}
+										autoComplete="username"
+										error={signUpErrors.name?.message}
+									/>
+								</div>
+								<div className="space-y-1">
+									<Label htmlFor="email">Email</Label>
+									<Input
+										name="email"
+										type="email"
+										{...signUpRegister("email")}
+										autoComplete="username"
+										error={signUpErrors.email?.message}
+									/>
+								</div>
+								<div className="space-y-1">
+									<Label htmlFor="password">Password</Label>
+									<Input
+										name="password"
+										type="password"
+										{...signUpRegister("password")}
+										autoComplete="current-password"
+										error={signUpErrors.password?.message}
+									/>
+								</div>
+								<div className="space-y-1">
+									<Label htmlFor="confirmPassword">Confirm Password</Label>
+									<Input
+										name="confirm-password"
+										type="password"
+										{...signUpRegister("confirmPassword")}
+										autoComplete="current-password"
+										error={signUpErrors.confirmPassword?.message}
+									/>
+								</div>
+							</CardContent>
+							<CardFooter>
+								<Button className="grow" type="submit" disabled={isLoading}>
+									{isLoading ? <LoadingIcon className="animate-spin" /> : null}
+									Sign up
+								</Button>
+							</CardFooter>
+						</Card>
+					</form>
+				</TabsContent>
+			</Tabs>
+		</div>
+	);
 }
