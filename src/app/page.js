95ad5651f0e40af874d1099b7fc1d3fdfd0d9853
xibluespider <xibluespider@gmail.com>
2024-12-28 16:37:03 +0530

    fix: app/page,AppSidebar: resolve LargestContentfulPaint warning

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import AppLogo from "../../public/bird.jpg";
 export default function Page() {
 	return (
 		<div className="grow border rounded-lg flex flex-col items-center justify-center space-y-3">
-			<Image src={AppLogo} alt="AppLogo" className="max-w-[80px] rounded-2xl" />
+			<Image src={AppLogo} alt="AppLogo" priority className="max-w-[80px] rounded-2xl" />
 			<div className="text-xl">kindly select a conversation</div>
 		</div>
 	);
