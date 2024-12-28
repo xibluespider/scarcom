c2d5884d67311c7f6601cf69dd1ce520459a1bae
xibluespider <xibluespider@gmail.com>
2024-12-28 15:55:14 +0530

    chore: db.js: prettier format

--- src/lib/db.js
+++ src/lib/db.js
@@ -8,11 +8,11 @@ const pool = postgres(process.env.DATABASE_URL, { max: 1 });
 export const db = drizzle(pool);
 
 export async function getUserByEmail(email) {
-  const iusers = await db.select().from(users).where(eq(users.email, email));
-  const [user] = iusers;
-  return user;
+	const iusers = await db.select().from(users).where(eq(users.email, email));
+	const [user] = iusers;
+	return user;
 }
 
 export async function addUser(credentials) {
-  await db.insert(users).values(credentials);
+	await db.insert(users).values(credentials);
 }
