0d510cb45aaa408fbe20d66ab5cf198b6133de91
xibluespider <xibluespider@gmail.com>
2024-12-28 15:57:09 +0530

    feat: db/searchUser: implement

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 
 import { users } from "./db-schema";
-import { eq } from "drizzle-orm";
+import { eq, or, like } from "drizzle-orm";
 
 const pool = postgres(process.env.DATABASE_URL, { max: 1 });
 export const db = drizzle(pool);
@@ -15,4 +15,16 @@ export async function getUserByEmail(email) {
 
 export async function addUser(credentials) {
 	await db.insert(users).values(credentials);
+}
+
+export async function searchUser(query) {
+	const iusers = await db
+		.select({
+			name: users.name,
+			email: users.email,
+		})
+		.from(users)
+		.where(or(like(users.name, `%${query}%`), like(users.email, `%${query}%`)));
+
+	return iusers;
 }
