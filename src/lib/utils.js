dedcf2ef93fb057ad328e5c1b1b9dec809dadc46
xibluespider <xibluespider@gmail.com>
2024-12-29 07:03:07 +0530

    feat: utils/VisibitilyWrapper: implement

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs) {
 	return twMerge(clsx(inputs));
 }
+
+export function VisibilityWrapper({ isHide, children }) {
+	if (isHide) return null;
+	return children;
+}
