753f026f300b6568b6b0ae3a240c57367ae5a196
xibluespider <xibluespider@gmail.com>
2025-01-17 10:58:20 +0530

    chore: db: globalMessages: refactor add, implement delete

--- src/lib/db.js
+++ src/lib/db.js
@@ -38,28 +38,28 @@ export async function searchUser(query) {
 }
 
 export async function addMessageToGlobal(payload) {
-	await db.insert(schema.globalMessages).values(payload).onConflictDoUpdate({
-		target: schema.globalMessages.id,
-		set: payload,
-	});
+	await db
+		.insert(schema.globalMessages)
+		.values(payload)
+		.onConflictDoUpdate({
+			target: schema.globalMessages.ringId,
+			set: { ...payload, createdAt: orm.sql`NOW()` },
+		});
 }
 
 export async function getGlobalMessages() {
 	const initialGlobalMessages = await db
-
 		.select({
 			username: schema.users.username,
 			message: schema.globalMessages.message,
+			messageId: schema.globalMessages.messageId,
 			createdAt: schema.globalMessages.createdAt,
 		})
-
 		.from(schema.globalMessages)
-
 		.innerJoin(
 			schema.users,
 			orm.eq(schema.globalMessages.userId, schema.users.id)
 		)
-
 		.orderBy(orm.asc(schema.globalMessages.createdAt));
 
 	return initialGlobalMessages;
@@ -72,4 +72,10 @@ export async function getUserByUsername(username) {
 		.where(orm.eq(schema.users.username, username));
 	const [user] = iusers;
 	return user;
+}
+
+export async function deleteGlobalMessage(messageId) {
+	await db
+		.delete(schema.globalMessages)
+		.where(orm.eq(schema.globalMessages.messageId, messageId));
 }
