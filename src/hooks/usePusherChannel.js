bd244dc9cc74db3041b3f8733187867883cac654
xibluespider <xibluespider@gmail.com>
2025-02-06 16:43:17 +0530

    refactor: decouple usePusherChannel from message state management

--- src/hooks/usePusherChannel.js
+++ src/hooks/usePusherChannel.js
@@ -1,33 +1,8 @@
-import { useState, useEffect } from "react";
-
 import Pusher from "pusher-js";
 
 export default function usePusherChannel(channelName) {
-	const [messages, setMessages] = useState([]);
-
-	const newMessageEventEffect = (message) => {
-		setMessages((prevState) => [...prevState, message]);
-		console.log(messages);
-	};
-
-	const deleteMessageEventEffect = ({ messageId }) => {
-		setMessages((prevMessages) =>
-			prevMessages.filter((message) => message.messageId !== messageId)
-		);
-	};
-
-	const pusherEffect = () => {
-		const options = { cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER };
-		const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, options);
-
-		const channel = pusher.subscribe(channelName);
-		channel.bind("new_message", newMessageEventEffect);
-		channel.bind("delete_message", deleteMessageEventEffect);
-
-		return () => pusher.unsubscribe(channelName);
-	};
-
-	useEffect(pusherEffect, [channelName]);
-
-	return messages;
+	const options = { cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER };
+	const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, options);
+	const pusherRef = pusher.subscribe(channelName);
+	return { pusherRef };
 }
